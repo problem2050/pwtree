9b46c659d6a5392a6d5399c907eda829c78f0dbd
problem2050 <759985819@qq.com>
2017-08-11 21:49:32 +0800

    modify db class

--- html/db.php
+++ html/db.php
@@ -1,14 +1,29 @@
 <?php
 header('Content-Type: text/html; charset=utf-8');
-require_once($_SERVER["Root_Path"]."/lib/Db/Mysqli.php");
+
+//var_dump($_SERVER['Root_Path']);
+require_once($_SERVER["Root_Path"]."/inc/bootstrap.php");
 
 
-$re =  ConnMysqli::getIntance()->queryOne("select * from test");
+//$re =  Db_Mysqli::getIntance()->queryfetch("select * from test");
 
+//var_dump($re);
+
+//foreach($re as $k=>$v){
+	
+	//var_dump($k,$v);
+//  echo $v['label'];
+//}
+//$re =  Db_Mysqli::getIntance()->query("insert into test(id,label)values(5,'vv')");
+$re =  Db_Mysqli::getIntance()->queryfetch("select * from test");
 var_dump($re);
 
 //var_dump(ConnMysqli::getIntance()->getInsertid());
+//var_dump(11);
+//$re = Db_Mysqli::getIntance()->queryPrepared("select * from test where id = ?",[5]);
+//var_dump($re);
 
+//var_dump($SITE_CONFIGS);
 
 ?>
 
--- lib/Db/Mysqli.php
+++ lib/Db/Mysqli.php
@@ -1,5 +1,5 @@
 <?php
-class ConnMysqli{
+class Db_Mysqli {
  
   private static $instance=false;
   private $host;
@@ -15,14 +15,15 @@ class ConnMysqli{
     $this->host = $config['host'] ? $config['host'] : 'localhost';
     $this->port = $config['port'] ? $config['port'] : '3306';
     $this->user = $config['user'] ? $config['user'] : 'root';
-    $this->pass = $config['password'] ? $config['password'] : '1';
+    $this->pass = $config['password'] ? $config['password'] : '';
     $this->db = $config['dbname'] ? $config['dbname'] : 'pwtree';
     //$this->charset=isset($arr['charset']) ? $arr['charset'] : 'utf8';
-       
+    $this->conn=null;
+    
     $this->conn=mysqli_connect($this->host,$this->user,$this->pass,$this->db);
     if(!$this->conn){
       SeasLog::log(SEASLOG_ERROR,"数据库连接失败");
-      SeasLog::log(SEASLOG_ERROR,"错误编码".mysqli_errno($this->conn));
+      SeasLog::log(SEASLOG_ERROR,"错误编码:".mysqli_errno($this->conn));
       SeasLog::log("错误信息".mysqli_error($this->conn));
       exit;
     }
@@ -45,20 +46,70 @@ class ConnMysqli{
    	                    'dbname'=>$conf[3],
    	                    'port'=>$conf[4]);
    	                    
-   	 self::$instance[$dbname] = new ConnMysqli($db_conf);   	      
+   	 self::$instance[$dbname] = new Db_Mysqli($db_conf);   	      
    }
     return self::$instance[$dbname];
   }
-    //执行sql语句的方法
+
   public function queryOne($sql){
-     $result=$this->conn->query($sql, MYSQLI_USE_RESULT);
-       while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
-      
-      }
-     //$row = $res-> mysqli_fetch_all(MYSQLI_ASSOC); 
-     //return $row;
+  	
+     $result =$this->conn->query($sql);    
+     $row = $result->fetch_array(MYSQLI_ASSOC);   
+     $result->close();
+       
+     return $row;
    }
    
+  public function query($sql){
+  	
+    $result = $this->conn->query($sql);
+    if($result===True){
+     	return True;
+    }else{
+    	$rows  = $result->num_rows;
+      $result->close();       
+      return $rows;     
+   }
+ }
+  
+  public function queryfetch($sql){
+  	 $datalist = [];
+     $result=$this->conn->query($sql);
+       while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
+         $datalist[] = $row;
+      }
+      
+      $result->close();
+       
+     return $datalist;
+   }
+         
+		
+		 public function execPrepared($sql, array $args)
+    {
+    	 
+        $stmt   = $this->conn->prepare($sql);
+        
+        $params = [];
+        $types  = array_reduce($args, function ($string, &$arg) use (&$params) {
+            $params[] = &$arg;
+            if (is_float($arg))         $string .= 'd';
+            elseif (is_integer($arg))   $string .= 'i';
+            elseif (is_string($arg))    $string .= 's';
+            else                        $string .= 'b';
+            return $string;
+        }, '');
+        array_unshift($params, $types);
+
+        call_user_func_array([$stmt, 'bind_param'], $params);        
+        $result = $stmt->execute() ;
+
+        $stmt->close();
+       
+       return $result;
+    
+		}
+		
    
    /**
     * 获得最后一条记录id
@@ -78,41 +129,16 @@ class ConnMysqli{
      $query=$this->query($sql);
       return mysqli_free_result($query);
     }
-    
-    /**
-     * 获取一行记录,return array 一维数组
-     */    
-    public function getRow($sql,$type="assoc"){
-     $query=$this->query($sql);
-     if(!in_array($type,array("assoc",'array',"row"))){
-       die("mysqli_query error");
-     }
-     
-     $funcname="mysqli_fetch_".$type;
-     
-     return $funcname($query);
-    }
-    
-    /**
-    * @author bin
-    * 获取一条记录,前置条件通过资源获取一条记录
-    */
-    public function getFormSource($query,$type="assoc"){
-    if(!in_array($type,array("assoc","array","row")))
-    {
-      die("mysqli_query error");
-    }
-    $funcname="mysqli_fetch_".$type;
-    return $funcname($query);
-    }
-    
+        
     
     public function setCommit($isbool)
     {
     	$this->conn->autocommit($isbool);
     }
      
-     
+    function __destruct(){
+        $this->conn->close();
+    }
      
 }
 ?>
